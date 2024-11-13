c22259cb7d6d8a84cb31b042f4f264066c5d2d88
Ducinviciable <ductelecompro@gmail.com>
2024-11-14 03:11:12 +0700

    feat: login page

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,25 @@
+import Footer from "@/pages/layouts/Footer";
+import Header from "@/pages/layouts/Header";
+
+const LoginPage = () => {
+    return (
+        <>
+            <Header />
+            <section>
+                <div className="container mx-auto flex flex-col items-center">
+                    {/* Image */}
+                    <div>
+
+                    </div>
+                    {/* Form */}
+                    <div>
+
+                    </div>
+                </div>
+            </section>
+            <Footer />
+        </>
+    );
+};
+
+export default LoginPage;
